ee36dfd62570da3b9191d393d3928201dd27aee5
adelss12 <iadelss12@gmail.com>
2026-07-26 22:09:27 +0300

    Add Windows desktop packaging with Electron

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 1100,
+        height: 780,
+        minWidth: 500,
+        minHeight: 650,
+        autoHideMenuBar: true,
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: false
+        }
+    });
+
+    mainWindow.loadFile("index.html");
+}
+
+app.whenReady().then(function () {
+    createWindow();
+
+    app.on("activate", function () {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
